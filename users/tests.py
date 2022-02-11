2fbc31a8037e0eef6e7ece89552419a052b36320
MousaNageh <200moussa200@gmail.com>
2022-02-11 16:50:48 +0200

    add test cases for login .

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,79 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from users.models import User 
+from faker import Faker
 
-# Create your tests here.
+class LoginAPI(APITestCase):
+  def setUp(self) :
+      self.fake =Faker()  
+      self.loginUrl = reverse("login")
+      self.email_and_password = self.fake.email()
+      self.user = User.objects.create(
+      email=self.email_and_password,
+      username=self.email_and_password,
+      first_name=self.fake.name(),
+      last_name=self.fake.name(),
+      )
+      self.user.set_password(self.email_and_password)
+      self.user.save()
+      return super().setUp()
+  def tearDown(self) :
+      return super().tearDown()
+
+
+
+class TestView(LoginAPI):
+
+  def test_empty_data(self):
+    data = {}
+    response =  self.client.post(self.loginUrl,data)
+    self.assertEqual(response.status_code , 400) 
+  
+  def test_empty_data_email(self):
+    data = {
+      "email":"",
+      "password":self.email_and_password
+    }
+    response =  self.client.post(self.loginUrl,data)
+    self.assertEqual(response.status_code , 400) 
+  
+  def test_empty_data_password(self):
+    data = {
+      "email":self.email_and_password,
+      "password":""
+    }
+    response =  self.client.post(self.loginUrl,data)
+    self.assertEqual(response.status_code , 400) 
+  
+  
+  def test_not_valid_email(self):
+    data = {
+      "email":"dsfcdsdsd",
+      "password":"12234353454"
+    }
+    response =  self.client.post(self.loginUrl,data)
+    self.assertEqual(response.status_code , 400) 
+  
+  
+  def test_not_valid_cardintional(self):
+    data = {
+      "email":"dsfcdsdsd@dsds.com",
+      "password":"12234353454"
+    }
+    response =  self.client.post(self.loginUrl,data)
+    self.assertEqual(response.status_code , 401) 
+  
+  
+  def test_valid_cardintional(self):
+    data = {
+      "email":self.email_and_password,
+      "password":self.email_and_password
+    }
+    response =  self.client.post(self.loginUrl,data)
+    self.assertEqual(response.status_code , 200) 
+
+  
+
+  
+
+
